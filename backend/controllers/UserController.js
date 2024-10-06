6eb4d82031d2f2b89e8596ceb71b9c03a4313b66
kalyanasundar8 <sundar31100@gmail.com>
2024-10-06 12:36:46 +0530

    OTP verification API added

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,12 +1,13 @@
 import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
 import User from "../models/UserModel.js";
 import { mobileAndEmailCheck } from "../services/Validation.js";
 import { generateOtp } from "../services/GenerateOtp.js";
 import { generateToken } from "../services/GenerateToken.js";
 
-// Desc    Create an user account
-// Route   /api/user/createAccount
-const createAccount = asyncHandler(async (req, res) => {
+// Desc    Create an user profile
+// Route   /api/user/createProfile
+const createProfile = asyncHandler(async (req, res) => {
   // Get the user input
   const { userName, mobileNumber, email, address } = req.body;
 
@@ -46,8 +47,49 @@ const createAccount = asyncHandler(async (req, res) => {
       return res.status(400).json({ message: "Something went wrong!" });
     }
   } else {
-    return res.status(400).json({ message: "User already exists, Try different mobilenumber or email" });
+    return res.status(400).json({
+      message: "User already exists, Try different mobilenumber or email",
+    });
   }
 });
 
-export { createAccount };
+// Desc    Verify OTP
+// Route   /api/user/verifyOtp
+const verifyOtp = asyncHandler(async (req, res) => {
+  const { otp, token } = req.body;
+  // Decode a token
+  let decodedToken;
+
+  try {
+    decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid (or) Expired Token" });
+  }
+
+  // Check the user verification status
+  const verifiedUser = await User.findOne({
+    _id: decodedToken.id,
+    verified: false,
+  });
+
+  if (!verifiedUser) {
+    return res.status(400).json({
+      message: "User already verified or does not exist, Please Signin.",
+    });
+  }
+
+  // Check the otp is valid
+  const validOtp = verifiedUser.otp === otp;
+  if (validOtp) {
+    verifiedUser.verified = true;
+    await verifiedUser.save();
+
+    return res.status(200).json({ token });
+  } else {
+    return res
+      .status(400)
+      .json({ message: "OTP is not valid, Please check your OTP" });
+  }
+});
+
+export { createProfile, verifyOtp };
