7f9d0d25b56d13d17582fc4f8219e2615df4ec49
sundar8 <kalyanasundar8>
2024-10-07 17:01:44 -0400

    Signin route added to the UserRoutes

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createProfile,
   setPassword,
+  signin,
   verifyOtp,
 } from "../controllers/UserController.js";
 
@@ -10,5 +11,6 @@ const userRouter = express.Router();
 userRouter.post("/createProfile", createProfile);
 userRouter.post("/verifyOtp", verifyOtp);
 userRouter.post("/setPassword", setPassword);
+userRouter.post("/signin", signin);
 
 export default userRouter;
